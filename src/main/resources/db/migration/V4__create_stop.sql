62800c0aa3bdf393e5a7432f1114db2f239565b4
Fareez Mir <fareezelahi@gmail.com>
2026-08-27 18:14:58 -0400

    Add stop table

---
+++ src/main/resources/db/migration/V4__create_stop.sql
@@ -0,0 +1,9 @@
+CREATE TABLE stop (
+    id BIGSERIAL PRIMARY KEY,
+    day_id BIGINT REFERENCES day(id),
+    name TEXT NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    dwell_minutes INTEGER NOT NULL,
+    dwell_source TEXT NOT NULL DEFAULT 'ESTIMATED'
+);
