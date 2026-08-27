7e1b42713adbcd8216f786c298dafe6043ff3df7
Fareez Mir <fareezelahi@gmail.com>
2026-08-27 19:47:36 -0400

    updated day table to include new fields

--- src/main/resources/db/migration/V3__create_day.sql
+++ src/main/resources/db/migration/V3__create_day.sql
@@ -1,5 +1,12 @@
 CREATE TABLE day (
     id BIGSERIAL PRIMARY KEY,
     leg_id BIGINT NOT NULL REFERENCES leg(id),
-    day_date DATE NOT NULL
+    day_date DATE NOT NULL,
+    start_lat DOUBLE PRECISION NOT NULL,
+    start_lon DOUBLE PRECISION NOT NULL,
+    end_lat DOUBLE PRECISION,
+    end_lon DOUBLE PRECISION,
+    start_time TIME NOT NULL,
+    latest_end_time TIME NOT NULL,
+    max_minutes_per_day INTEGER NOT NULL
 );
